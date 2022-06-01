731143e03e2fba120033466557834ac2a50ca928
eric373 <25942990+eric373@users.noreply.github.com>
2022-05-31 20:23:04 -0400

    Basic use of argparse module

---
+++ useArgparse.py
@@ -0,0 +1,24 @@
+#python useArgparse.py foo 3 --optional val1 --optionalFlag -of2 --restrictedOption 
+import subprocess as sp
+import argparse
+if __name__ == "__main__":
+    sp.call("cls", shell = True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("arg1", help = "Description of first argument")
+    parser.add_argument("num", type = int, help = "Any number")
+    parser.add_argument("--optional", help = "An optional argument")
+    parser.add_argument("--optionalFlag", action = "store_true", help = "An optional flag")
+    parser.add_argument("-of2", "--optionalFlag2", action = "store_true", help = "Another optional flag")
+    parser.add_argument("-ro", "--restrictedOption", choices = ["a", "b", "c"], help = "One of [a, b, c]")
+    args = parser.parse_args()
+
+    print(f"First positional argument is {args.arg1}")
+    print(f"Second positioning argument is {args.num}")
+    if args.optional:
+        print(f"Optional parameter: {args.optional}")
+    if args.optionalFlag:
+        print(f"Optional flag enabled")
+    if args.optionalFlag2:
+        print(f"Optional flag2 enabled")
+    if args.restrictedOption:
+        print(f"Restricted option value: {args.restrictedOption}")
