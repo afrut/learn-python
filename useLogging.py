2845ef47a11eeb6ae1e36b26c2a24ad35fef86c8
eric373 <25942990+eric373@users.noreply.github.com>
2022-06-30 10:26:49 -0400

    Added milliseconds to loggign

--- useLogging.py
+++ useLogging.py
@@ -3,7 +3,7 @@ import logging
 
 if __name__ == "__main__":
     # Format for timestamp and logging message
-    fmt = "%(asctime)s: %(message)s"
+    fmt = "%(asctime)s.%(msecs)03d: %(message)s"
     datefmt = "%Y-%m-%d %H:%M:%S"
     logging.basicConfig(format = fmt, level = logging.INFO, datefmt = datefmt)
     logging.info("hello world")
