db78d62d8f17bba0cfdad638a31c23f62c103ceb
eric373 <25942990+eric373@users.noreply.github.com>
2023-09-09 22:34:54 -0400

    Basic usage of glob for pathnames

---
+++ _glob/main.py
@@ -0,0 +1,33 @@
+# Pattern matching file paths and names using Unix shell rules
+import glob
+import os
+
+if __name__ == "__main__":
+    # Get root of the repository via relative directory
+    this_file = os.path.abspath(__file__)
+    this_file_dir = os.path.dirname(this_file)
+    repo_root = os.path.abspath(f"{this_file_dir}\\..")
+
+    # All files under a directory
+    pattern = f"{repo_root}\\*"
+    print(pattern)
+    list(map(lambda x: print(f"    {x}"), sorted(glob.glob(pattern))))
+    print("")
+
+    # All files under a directory matching an extension
+    pattern = f"{repo_root}\\*.py"
+    print(pattern)
+    list(map(lambda x: print(f"    {x}"), sorted(glob.glob(pattern))))
+    print("")
+
+    # All directories in a current directory
+    pattern = f"{repo_root}\\**\\"
+    print(pattern)
+    list(map(lambda x: print(f"    {x}"), sorted(glob.glob(pattern))))
+    print("")
+
+    # All files and subdirectories in a directory and it subdirectories
+    pattern = f"{repo_root}\\**"
+    print(pattern)
+    list(map(lambda x: print(f"    {x}"), sorted(glob.glob(pattern, recursive = True))))
+    print("")
