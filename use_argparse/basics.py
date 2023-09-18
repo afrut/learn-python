dcad3ac80760b53a3440675af6358cfa4fe62e7f
eric373 <25942990+eric373@users.noreply.github.com>
2023-09-17 23:38:32 -0400

    black-formatted

--- use_argparse/basics.py
+++ use_argparse/basics.py
@@ -1,22 +1,30 @@
-#python basics.py foo 3 --required foo --optional val1 --optionalFlag -of2 --restrictedOption a
-#python basics.py foo 3 --required foo --optional val1 --optionalFlag -of2 --restrictedOption a --optionalWithDefault
-#python basics.py foo 3 --required foo --optional val1 --optionalFlag -of2 --restrictedOption a --optionalWithDefault someValue
-import subprocess as sp
+# python basics.py foo 3 --required foo --optional val1 --optionalFlag -of2 --restrictedOption a
+# python basics.py foo 3 --required foo --optional val1 --optionalFlag -of2 --restrictedOption a --optionalWithDefault
+# python basics.py foo 3 --required foo --optional val1 --optionalFlag -of2 --restrictedOption a --optionalWithDefault someValue
 import argparse
+import subprocess as sp
 
-
-    
 if __name__ == "__main__":
-    sp.call("cls", shell = True)
+    sp.call("cls", shell=True)
     parser = argparse.ArgumentParser()
-    parser.add_argument("arg1", help = "Description of first argument")
-    parser.add_argument("num", type = int, help = "Any number")
-    parser.add_argument("--optional", help = "An optional argument")
-    parser.add_argument("--optionalFlag", action = "store_true", help = "An optional flag")
-    parser.add_argument("-of2", "--optionalFlag2", action = "store_true", help = "Another optional flag")
-    parser.add_argument("-ro", "--restrictedOption", choices = ["a", "b", "c"], help = "One of [a, b, c]")
-    parser.add_argument("--optionalWithDefault", nargs = "?", const = "valueWhenSpecifiedButNoValue", type = str, default = "valueWhenNotSpecified")
-    parser.add_argument("-r", "--required", type = str, required = True)
+    parser.add_argument("arg1", help="Description of first argument")
+    parser.add_argument("num", type=int, help="Any number")
+    parser.add_argument("--optional", help="An optional argument")
+    parser.add_argument("--optionalFlag", action="store_true", help="An optional flag")
+    parser.add_argument(
+        "-of2", "--optionalFlag2", action="store_true", help="Another optional flag"
+    )
+    parser.add_argument(
+        "-ro", "--restrictedOption", choices=["a", "b", "c"], help="One of [a, b, c]"
+    )
+    parser.add_argument(
+        "--optionalWithDefault",
+        nargs="?",
+        const="valueWhenSpecifiedButNoValue",
+        type=str,
+        default="valueWhenNotSpecified",
+    )
+    parser.add_argument("-r", "--required", type=str, required=True)
     args = parser.parse_args()
 
     print(f"First positional argument is {args.arg1}")
