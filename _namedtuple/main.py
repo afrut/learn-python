07cd7020cda72ac12996300cbf6872340dfb4f39
eric373 <25942990+eric373@users.noreply.github.com>
2023-10-17 19:58:05 -0400

    Create record types

--- _namedtuple/main.py
+++ _namedtuple/main.py
@@ -1,3 +1,4 @@
+import typing as t
 from collections import namedtuple
 
 # Create a class Vector3D
@@ -9,6 +10,13 @@ def some_func():
     return (1, 2, 3)
 
 
+# Define a record type by inheriting from NamedTuple
+# Equivalent to using Vector2D = namedtuple(typename = "Vector2D", field_names = ["x", "y"])
+class Vector2D(t.NamedTuple):
+    x: int = 0
+    y: int = 0
+
+
 if __name__ == "__main__":
     v = Vector3D(1, 1)
     v.x  # value of x
@@ -18,3 +26,6 @@ if __name__ == "__main__":
     # Cast the output of returns into the record class
     v = Vector3D._make(some_func())
     v._asdict()  # Get dictionary representation of the class
+
+    v = Vector2D()
+    assert v.x + v.y == 0, f"{v.x} + {v.y} was expected to be 0 + 0 = 0"
