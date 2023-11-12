3067af7f439f4b4da5b2acffbf4ec04330e78d28
eric373 <25942990+eric373@users.noreply.github.com>
2023-11-11 23:21:08 -0500

    Unnecessary

--- printAttr.py
+++
@@ -1,9 +0,0 @@
-# Print the members of any object
-def printAttr(x):
-    print(f"Type: {type(x)}")
-    for attr in sorted(dir(x)):
-        print(f"    {attr}")
-
-if __name__ == "__main__":
-    ls = []
-    printAttr(ls)
