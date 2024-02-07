4cf46d44601e1e725673b5a1d4319111c3a595e5
eric373 <25942990+eric373@users.noreply.github.com>
2024-02-07 15:49:42 -0500

    Add logging format

--- useLogging.py
+++ useLogging.py
@@ -1,9 +1,10 @@
-#python useLogging.py
+# python useLogging.py
 import logging
 
 if __name__ == "__main__":
     # Format for timestamp and logging message
     fmt = "%(asctime)s.%(msecs)03d: %(message)s"
+    # fmt = "%(pathname)s:%(lineno)d: %(message)s" # path of the file with line number of every log
     datefmt = "%Y-%m-%d %H:%M:%S"
-    logging.basicConfig(format = fmt, level = logging.INFO, datefmt = datefmt)
+    logging.basicConfig(format=fmt, level=logging.INFO, datefmt=datefmt)
     logging.info("hello world")
