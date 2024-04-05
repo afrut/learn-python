8fa3f63b2a6724bb9bf5522dbb053c3d3bf9dea5
eric373 <25942990+eric373@users.noreply.github.com>
2024-04-05 13:01:28 -0400

    Improved

--- decorators/main.py
+++ decorators/main.py
@@ -31,17 +31,23 @@ def print_return_before_returning(func: Callable) -> Callable:
     return wrapper
 
 
-# Functions that return functions can be chain-called like this
+# region Functions that return functions can be chain-called like this
 # prints (3, 3, 3) and returns (3, 3, 3)
+print("Functions that return functions")
 print_return_before_returning(add_3d_vectors)(tpl3d1, tpl3d2)
-
 
 # Get a version of a function that prints the return before returning
 add_3d_vectors = print_return_before_returning(add_3d_vectors)
 add_3d_vectors(tpl3d1, tpl3d2)  # prints (3, 3, 3) and returns (3, 3, 3)
+print("")
+# endregion
 
 
-# The same function as print_before_returning but uses the wraps decorator
+# region The same function as print_before_returning but uses the wraps
+# decorator
+print("Using the wraps decorator")
+
+
 def print_return(func: Callable) -> Callable:
     # The wraps decorator preserves the original function's introspection
     # metadata, like __name__
@@ -68,9 +74,11 @@ add_2d_vectors(tpl2d1, tpl2d2)
 # Using wraps decorator
 add_3d_vectors.__name__  # value is "wrapper", because wrapper calls add_3d_vectors
 add_2d_vectors.__name__  # value is "add_2d_vectors" because of @wraps decorator
+print("")
+# endregion
 
 
-# A simple decorator pattern
+# region A simple decorator pattern
 def simple_decorator(func: Callable):
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -82,27 +90,70 @@ def simple_decorator(func: Callable):
     return wrapper
 
 
-# A decorator that takes in an argument
+# endregion
+
+
+# region A decorator that takes in an argument
 # Note that it has 2 outer functions that return functions
-def repeat(num: int) -> Callable:
-    def log_before(func) -> Callable:
+print("Decorators that take an argument")
+
+
+def repeat_log_before_returning(num: int) -> Callable:
+    """
+    Repeats the log before returning
+    """
+
+    def log_before_returning(func) -> Callable:
+        """
+        Logs before returning as a demonstration function
+        """
+
         @wraps(func)
         def wrapper(*args, **kwargs):
+            ret = func(*args, **kwargs)
             for _ in range(num):
-                logging.info("Logging before")
-            func(*args, **kwargs)
+                logging.info(f"Logging before returning {ret}")
+            return ret
 
         return wrapper
 
-    return log_before
+    return log_before_returning
 
 
 # It can be chained like this
-repeat(3)(add_2d_vectors)(tpl2d1, tpl2d2)
+repeat_log_before_returning(3)(add_2d_vectors)(tpl2d1, tpl2d2)
 
-f = repeat(3)  # f is def log_before(func) -> Callable:
-g = f(add_2d_vectors)  # def wrapper(*args, **kwargs):
+f = repeat_log_before_returning(3)  # f is def log_before_returning(func) -> Callable
+g = f(add_2d_vectors)  # g is def wrapper(*args, **kwargs)
 h = g(tpl2d1, tpl2d2)  # h is (3, 3)
+print("")
+
+print("Using decorators that take arguments")
+
+
+# Use it as a decorator to repeat the log of the return before returning
+@repeat_log_before_returning(num=7)
+def add_2d_vectors_decorated(x, y):
+    return (x[0] + y[0], x[1] + y[1])
+
+
+# This will log the return 7 times before returning
+add_2d_vectors_decorated(tpl2d1, tpl2d2)
+
+
+# endregion
+
+
+# # Repeat the function 7 times
+# @repeat(num=7)
+# def add_2d_vectors_decorated(x: Tuple[int, int], y: Tuple[int, int]) -> Tuple[int, int]:
+#     return (x[0] + y[0], x[1] + y[1])
+
+
+# print(add_2d_vectors_decorated((1, 1), (2, 2)))
+
+
+# See json_schema_validation.py for validating input dict schemas.
 
 # decorators that take 0 or keyword args
 # nesting decorators
@@ -118,7 +169,7 @@ h = g(tpl2d1, tpl2d2)  # h is (3, 3)
 # creating singletons
 # caching return values
 # adding information - eg, units
-# validating inputs - eg, JSON schema
+# validating inputs
 
 # Deco
 # ShowMe
